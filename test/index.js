05193eb4d736f83d40145043ec32ef6657551679
haoxin <haoxins@outlook.com>
2017-02-16 15:13:56 +0800

    use arrow function

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+
 'use strict'
 
 const config = require('./config')
@@ -11,107 +12,107 @@ const fs = require('fs')
 let testfile = '1.png'
 let filepath = `testfiles/${Date.now()}-${testfile}`
 
-describe('## upyun', function() {
-  describe('# basic', function() {
-    it('getUsage()', function() {
+describe('## upyun', () => {
+  describe('# basic', () => {
+    it('getUsage()', () => {
       return yun.getUsage()
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
         assert(!isNaN(parseInt(result.body)))
       })
     })
 
-    it('putFile() - file path', function() {
+    it('putFile() - file path', () => {
       return yun.putFile(path.join(__dirname, testfile), filepath)
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
 
-    it('headFile()', function() {
+    it('headFile()', () => {
       return yun.headFile(filepath)
       .then(function(result) {
         equal(result.status, 200)
       })
     })
 
-    it('getFile()', function() {
+    it('getFile()', () => {
       return yun.getFile(filepath, path.join(__dirname, Date.now() + '.png'))
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
 
-    it('listBucket()', function() {
+    it('listBucket()', () => {
       return yun.listBucket('/')
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
         assert(result.body.length > 0)
-        result.body.forEach(function(item) {
+        result.body.forEach(item => {
           assert.deepEqual(Object.keys(item), ['bucket', 'type', 'size', 'modifyTime'])
         })
       })
     })
 
-    it('rmFile()', function() {
+    it('rmFile()', () => {
       return yun.headFile(filepath)
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
   })
 
-  describe('putFile() stream', function() {
+  describe('putFile() stream', () => {
     let filepath2 = filepath.replace('.png', '.2.png')
 
-    it('putFile()', function() {
+    it('putFile()', () => {
       let size = fs.statSync(path.join(__dirname, testfile)).size
 
       return yun.putFile(fs.createReadStream(path.join(__dirname, testfile)), filepath2, {
         size: size
       })
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
 
-    it('getFile()', function() {
+    it('getFile()', () => {
       return yun.getFile(filepath2, path.join(__dirname, Date.now() + '.2.png'))
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
 
-    it('rmFile()', function() {
+    it('rmFile()', () => {
       return yun.headFile(filepath2)
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
   })
 
-  describe('putFile() buffer', function() {
+  describe('putFile() buffer', () => {
     let filepath3 = filepath.replace('.png', '.3.png')
 
-    it('putFile()', function() {
+    it('putFile()', () => {
       let filedata = fs.readFileSync(path.join(__dirname, testfile))
 
       return yun.putFile(filedata, filepath3)
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
 
-    it('getFile()', function() {
+    it('getFile()', () => {
       return yun.getFile(filepath3, path.join(__dirname, Date.now() + '.3.png'))
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
 
-    it('rmFile()', function() {
+    it('rmFile()', () => {
       return yun.headFile(filepath3)
-      .then(function(result) {
+      .then(result => {
         equal(result.status, 200)
       })
     })
