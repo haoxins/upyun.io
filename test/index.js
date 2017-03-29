60e20d2c470bc8b18e701d14edbdd06a97fdce0c
haoxin <haoxins@outlook.com>
2017-03-29 14:58:27 +0800

    use modern javascript

--- test/index.js
+++ test/index.js
@@ -9,112 +9,88 @@ const upyun = require('..')
 const yun = upyun(config)
 const fs = require('fs')
 
-let testfile = '1.png'
-let filepath = `testfiles/${Date.now()}-${testfile}`
+const testfile = '1.png'
+const filepath = `testfiles/${Date.now()}-${testfile}`
 
 describe('## upyun', () => {
   describe('# basic', () => {
-    it('getUsage()', () => {
-      return yun.getUsage()
-      .then(result => {
-        equal(result.status, 200)
-        assert(!isNaN(parseInt(result.body)))
+    it('getUsage()', async () => {
+      const result = await yun.getUsage()
+      equal(result.status, 200)
+      assert(!isNaN(parseInt(result.body)))
+    })
+
+    it('putFile() - file path', async () => {
+      const result = await yun.putFile(path.join(__dirname, testfile), filepath)
+      equal(result.status, 200)
+    })
+
+    it('headFile()', async () => {
+      const result = await yun.headFile(filepath)
+      equal(result.status, 200)
+    })
+
+    it('getFile()', async () => {
+      const result = await yun.getFile(filepath, path.join(__dirname, Date.now() + '.png'))
+      equal(result.status, 200)
+    })
+
+    it('listBucket()', async () => {
+      const result = await yun.listBucket('/')
+      equal(result.status, 200)
+      assert(result.body.length > 0)
+      result.body.forEach(item => {
+        assert.deepEqual(Object.keys(item), ['bucket', 'type', 'size', 'modifyTime'])
       })
     })
 
-    it('putFile() - file path', () => {
-      return yun.putFile(path.join(__dirname, testfile), filepath)
-      .then(result => {
-        equal(result.status, 200)
-      })
-    })
-
-    it('headFile()', () => {
-      return yun.headFile(filepath)
-      .then(function(result) {
-        equal(result.status, 200)
-      })
-    })
-
-    it('getFile()', () => {
-      return yun.getFile(filepath, path.join(__dirname, Date.now() + '.png'))
-      .then(result => {
-        equal(result.status, 200)
-      })
-    })
-
-    it('listBucket()', () => {
-      return yun.listBucket('/')
-      .then(result => {
-        equal(result.status, 200)
-        assert(result.body.length > 0)
-        result.body.forEach(item => {
-          assert.deepEqual(Object.keys(item), ['bucket', 'type', 'size', 'modifyTime'])
-        })
-      })
-    })
-
-    it('rmFile()', () => {
-      return yun.headFile(filepath)
-      .then(result => {
-        equal(result.status, 200)
-      })
+    it('rmFile()', async () => {
+      const result = await yun.headFile(filepath)
+      equal(result.status, 200)
     })
   })
 
   describe('putFile() stream', () => {
-    let filepath2 = filepath.replace('.png', '.2.png')
+    const filepath2 = filepath.replace('.png', '.2.png')
 
-    it('putFile()', () => {
-      let size = fs.statSync(path.join(__dirname, testfile)).size
+    it('putFile()', async () => {
+      const size = fs.statSync(path.join(__dirname, testfile)).size
 
-      return yun.putFile(fs.createReadStream(path.join(__dirname, testfile)), filepath2, {
-        size: size
+      const result = await yun.putFile(fs.createReadStream(path.join(__dirname, testfile)), filepath2, {
+        size
       })
-      .then(result => {
-        equal(result.status, 200)
-      })
+      equal(result.status, 200)
     })
 
-    it('getFile()', () => {
-      return yun.getFile(filepath2, path.join(__dirname, Date.now() + '.2.png'))
-      .then(result => {
-        equal(result.status, 200)
-      })
+    it('getFile()', async () => {
+      const result = await yun.getFile(filepath2, path.join(__dirname, Date.now() + '.2.png'))
+      equal(result.status, 200)
     })
 
-    it('rmFile()', () => {
-      return yun.headFile(filepath2)
-      .then(result => {
-        equal(result.status, 200)
-      })
+    it('rmFile()', async () => {
+      const result = await yun.headFile(filepath2)
+      equal(result.status, 200)
     })
   })
 
   describe('putFile() buffer', () => {
-    let filepath3 = filepath.replace('.png', '.3.png')
+    const filepath3 = filepath.replace('.png', '.3.png')
 
-    it('putFile()', () => {
-      let filedata = fs.readFileSync(path.join(__dirname, testfile))
+    it('putFile()', async () => {
+      const filedata = fs.readFileSync(path.join(__dirname, testfile))
 
-      return yun.putFile(filedata, filepath3)
-      .then(result => {
-        equal(result.status, 200)
-      })
+      const result = await yun.putFile(filedata, filepath3)
+      equal(result.status, 200)
     })
 
-    it('getFile()', () => {
-      return yun.getFile(filepath3, path.join(__dirname, Date.now() + '.3.png'))
-      .then(result => {
-        equal(result.status, 200)
-      })
+    it('getFile()', async () => {
+      const result = await yun.getFile(filepath3, path.join(__dirname, Date.now() + '.3.png'))
+      equal(result.status, 200)
     })
 
-    it('rmFile()', () => {
-      return yun.headFile(filepath3)
-      .then(result => {
-        equal(result.status, 200)
-      })
+    it('rmFile()', async () => {
+      const result = await yun.headFile(filepath3)
+      equal(result.status, 200)
     })
   })
 })
